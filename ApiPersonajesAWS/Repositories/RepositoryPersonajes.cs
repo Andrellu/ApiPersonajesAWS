ec98bb181876080e76c8f6fc233d4781c54c3fbc
andres <andrellu.o.f@gmail.com>
2022-04-27 11:23:58 +0200

    Add project files.

---
+++ ApiPersonajesAWS/Repositories/RepositoryPersonajes.cs
@@ -0,0 +1,41 @@
+﻿using ApiPersonajesAWS.Data;
+using ApiPersonajesAWS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPersonajesAWS.Repositories
+{
+    public class RepositoryPersonajes
+    {
+        private PersonajesContext context;
+
+        public RepositoryPersonajes(PersonajesContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Personaje> GetPersonajes()
+        {
+            return this.context.Personajes.ToList();
+        }
+
+        public Personaje FindPersonaje(int idPersonaje)
+        {
+            return this.context.Personajes.Where(x => x.IdPersonaje == idPersonaje).FirstOrDefault();
+        }
+
+        public void InsertPersonaje(int id, string nombre, string imagen)
+        {
+            Personaje personaje = new Personaje()
+            {
+                IdPersonaje = id,
+                Nombre = nombre,
+                Imagen = imagen
+            };
+            this.context.Personajes.Add(personaje);
+            this.context.SaveChanges();
+        }
+    }
+}
